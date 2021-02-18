524984c7c76b4653bd05a9a310b16414ddd02b60
Marcin Stelmach <stelmyit@gmail.com>
2021-02-18 19:56:33 +0100

    Spring boot

---
+++ src/main/java/com/stelmyit/skijumping/SkijumpingApplication.java
@@ -0,0 +1,13 @@
+package com.stelmyit.skijumping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkijumpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkijumpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stelmyit/skijumping/SkijumpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stelmyit.skijumping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkijumpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
